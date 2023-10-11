0134bc596d7c4d261b9c90a21d1f92b0b2239fe9
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-10-11 16:15:50 +0000

    added sphere.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created by: Finn Kitor
+# Created on: October 11th, 2023
+# This module contains the constants for the sphere program.
+
+PI = 3.14159265359
---
+++ sphere.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Created by: Finn Kitor
+# Created on: October 11th, 2023
+# This program asks the user for the radius of
+# a sphere in cm. It then calculates and displays
+# the surface area and volume of the sphere.
+import constants
+
+
+def main():
+    # user inputs the radius of the sphere into the terminal
+    radius = float(input("Enter the radius of the sphere (cm): "))
+
+    # terminal calculates the area and volume of the sphere
+    surface_area = 4 * constants.PI * radius * 2
+    volume = 4 / 3 * constants.PI * radius * 4
+    # terminal displays the surface area and volume of the sphere
+    print("")
+    print("The Surface Area is = {} cm^2".format(surface_area))
+    print("The Volume is = {} cm^3".format(volume))
+
+
+if __name__ == "__main__":
+    main()
